e019196d132a790309830a8a868f34c5c8f4c39c
storres8 <stevetorres7562@gmail.com>
2018-11-16 15:27:35 -0500

    added remove and show functions.

--- Part4_Array_Exercise.js
+++ Part4_Array_Exercise.js
@@ -2,5 +2,19 @@ var roster = []
 
 // adds a new student through a prompt \
 function addNew(){
-  let newName = prompt("Enter name you would like to add")
+  let newName = prompt("Enter name you would like to add");
+  roster.push(newName)
+}
+
+// removes a student from the roster
+function removeName(){
+  let deleteName = prompt("What name would you like to remove?")
+
+  let position = roster.indexOf(deleteName);
+
+  roster.splice(index,1)
+}
+
+function display(){
+  console.log(roster);
 }
